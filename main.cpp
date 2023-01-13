c97f8cfbe983d23458cc5a5e55748bf0d6877195
“Ilya <tsoi_ilya02@mail.ru>
2023-01-13 21:46:23 +0300

    new commit

--- main.cpp
+++ main.cpp
@@ -42,25 +42,38 @@ class Calculator{
 int main(){
     Calculator calc;
     double num1, num2;
+    bool input;
     while(true){
+        while(input == false){
         std::cout << "Enter number 1: " << std::endl;
         std::cin >> num1;
         calc.set_num1(num1);
-
+        if (calc.set_num1(num1) == false){
+            std::cout << "Неверный ввод" << std::endl;
+        }
+        else if(calc.set_num1(num1) == true){
+            input = true;
+        }
+        }
+        
+         while(input == true){
         std::cout << "Enter number 2: " << std::endl;
         std::cin >> num2;
         calc.set_num2(num2);
-        if(calc.set_num1(num1) == true && calc.set_num2(num2) == true){
+        if (calc.set_num2(num2) == false){
+            std::cout << "Неверный ввод" << std::endl;
+        }
+        else if(calc.set_num1(num1) == true){
+            input = false;
+        }
+        }
+
         std::cout << "num1 + num2 " << calc.add() << std::endl;
         std::cout << "num1 * num2 " << calc.multiply() << std::endl;
         std::cout << "num1 - num2 " << calc.subtract_1_2() << std::endl;
-        std::cout << "num2 - num1 " <<calc.subtract_2_1() << std::endl;
-        std::cout << "num1 / num2 " <<calc.divide_1_2() << std::endl;
-        std::cout << "num2 / num1 " <<calc.divide_2_1() << std::endl;
-        }
-        else{
-            std::cout << "Wrong number" << std::endl;
-        }        
+        std::cout << "num2 - num1 " << calc.subtract_2_1() << std::endl;
+        std::cout << "num1 / num2 " << calc.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 " << calc.divide_2_1() << std::endl;    
 
     }
 
